c293d0330c62eca4c7c80297d27ba729bd0c6f21
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-13 22:05:43 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -6,4 +6,21 @@ prompt.get('input', function (err, result) {
   
     // Write code here
   
+  function maxnum(a,b,c,d){
+    if(a>b && a>c && a>d){
+        console.log(a);
+    }
+    else if(b>a && b>c && b>d){
+        console.log(b);
+    }
+    else if(c>a && c>b && c>d){
+        console.log(c);
+    }
+    else if(d>a && d>c && d>b){
+        console.log(d);
+    }
+}
+
+maxnum(1,2,3,4);
+  
 });
