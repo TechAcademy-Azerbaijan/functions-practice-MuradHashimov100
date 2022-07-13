186103878680705b59b963223d01320854953ca6
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-13 22:38:54 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -6,4 +6,24 @@ prompt.get('input', function (err, result) {
   
     // Write code here
   
+  function primenum(num){
+    primenum = true;
+  for(let i = 2;i<num;i++){
+    if(num%i==0){
+      primenum = false;
+      break;
+    }
+    else{
+      primenum = true;
+    }
+  }
+  if(primenum==true){
+    console.log('Eded sadedir')
+  }
+  else{
+    console.log('Eded sade deyildir');
+    }
+}
+primenum(prompt());
+  
 });
