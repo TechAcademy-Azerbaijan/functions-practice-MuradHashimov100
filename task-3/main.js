1712f154ecce9cd17157a37d5fb4a9ef393a723d
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-13 22:32:35 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -5,5 +5,13 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+  function arrReverse(arr) {
+    for (i=arr.length-1; i>=0; i--) {
+      console.log(arr[i])
+    }
+  }
+  let arr1 = [ 'p0', 'p1', 'p2' ];
+  arrReverse(arr1);
+  arrReverse(arr2= [92846,96841,33582,25998]);
   
 });
