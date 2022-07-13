24f583577ce228ed88cf01086963d4a21de9ea5f
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-13 22:31:24 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -6,4 +6,15 @@ prompt.get('input', function (err, result) {
   
     // Write code here
   
+      function numberPow(num){
+            let pow = 1;
+            for(let i = 1;i<num;i++){
+               if(i**2 <num){
+                pow = i ** 2;
+                console.log(pow);
+               }
+            }
+        }
+        numberPow(20);
+  
 });
