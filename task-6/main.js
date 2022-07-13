ffb69a33bbdb48339d179b782bc0933d89903c1e
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-13 22:12:34 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -6,4 +6,11 @@ prompt.get('input', function (err, result) {
   
     // Write code here
   
+     function duzbucaqli(a,b){
+        return console.log(a*b);
+     }
+
+    duzbucaqli(3,8);
+    duzbucaqli(7,7);
+  
 });
