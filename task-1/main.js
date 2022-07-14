fc7e3bc3df734648b691165f8542453b1d4c7363
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-14 22:37:01 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -5,5 +5,16 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+   n ;
+      let a,b,c;
+      // debugger
+      a = parseInt( n / 100);
+      b  = parseInt(n % 100 / 20);
+      c = parseInt(n % 100 % 20);
+      console.log(a,b,c);
+      console.log(a * 100 + b * 30 + c * 2);
+      return n;
+    }
+    kompdisk(297);
   
 });
