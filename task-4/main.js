8f7bbac460014e2bb0290e00fc0126ad06cb86a9
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-15 00:57:54 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -5,5 +5,18 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+    
   
+     function fibonacci(num){
+        let n1 = 1, n2 = 2, nextStep;
+        for(let i = 1;i<=num;i++){
+            // console.log(i)
+            // debugger;
+            console.log(n1);
+            nextStep = n1 + n2;
+            n1 = n2;
+            n2 = nextStep;
+        }
+       }
+       fibonacci(10);
 });
